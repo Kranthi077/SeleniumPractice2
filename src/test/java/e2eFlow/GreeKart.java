3d3d040284b7c2638f86508340cf1f0cd069835c
Kranthi <111005858+Kranthi077@users.noreply.github.com>
2022-11-02 13:37:36 +0530

    update

--- src/test/java/e2eFlow/GreeKart.java
+++ src/test/java/e2eFlow/GreeKart.java
@@ -1,19 +1,24 @@
 package e2eFlow;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class GreeKart {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); // sets Chromediver.exe property in system
 		WebDriver d = new ChromeDriver(); // object of chromeDriver class referring to WebDriver Interface
+		d.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//d.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		d.manage().window().maximize();
 		// d.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
 		d.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -22,6 +27,21 @@ public class GreeKart {
 																					// Arrays.asList(words);
 		List<String> inputList = Arrays.asList(inputArray);
 
+		GreeKart.addItemsToCart(inputList, d);
+
+		GreeKart.verifyItemsInCart(inputList, d);
+
+		d.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+
+		d.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+		d.findElement(By.className("promoBtn")).click();
+		String promoMessage = d.findElement(By.className("promoInfo")).getText();
+		Assert.assertEquals(promoMessage, "Code applied ..!");
+
+		d.close();
+	}
+
+	public static void addItemsToCart(List<String> inputList, WebDriver d) throws InterruptedException {
 		List<WebElement> itemsList = d.findElements(By.xpath("//h4[@class='product-name']"));
 		List<WebElement> addToCartButtonList = d.findElements(By.xpath("//button[text()='ADD TO CART']"));
 		int j = 0;
@@ -30,27 +50,31 @@ public class GreeKart {
 			if (inputList.contains(itemFromBrowser)) {
 				addToCartButtonList.get(i).click();
 				j++;// 4
-				if (j == 4)//true
+				if (j == 4)// true
 					break;
 			}
 			System.out.println(itemFromBrowser);
 		}
 
-		Thread.sleep(3000);
 		
+	}
+
+	public static void verifyItemsInCart(List<String> inputList, WebDriver d) throws InterruptedException {
 		d.findElement(By.className("cart-icon")).click();
-		
-		String xpath = "//li[@class=\"cart-item\"]/descendant::p[@class=\"product-name\"]";
+		Thread.sleep(3000);
+		String xpath = "//li[@class='cart-item']/descendant::p[@class='product-name']";
 		List<WebElement> cartList = d.findElements(By.xpath(xpath));
-		for (int i = 0; i < cartList.size()/2; i++) {
+		for (int i = 0; i < 4; i++) {
 			
-			// = cartList.get(i).getText(
-			
+			Thread.sleep(3000);
+			String itemFromCart = cartList.get(i).getText().split(" ")[0].trim();
+			if (inputList.contains(itemFromCart)) {
+				System.out.println(itemFromCart + " Item is present in cart");
+			} else {
+				System.out.println("Item not present - Test failed");
+			}
+
 		}
-		
-		
-		
-		d.close();
 	}
 
 }
